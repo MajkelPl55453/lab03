cd21f77a59137abc021c7729907b13243d883da0
Michal-l\Majkel <mih.pago@op.pl>
2018-04-12 18:30:31 +0200

    Initial commit

---
+++ app/src/main/java/pl/lab03/lab03/Planeta.java
@@ -0,0 +1,20 @@
+package pl.lab03.lab03;
+
+/**
+ * Created by Majkel on 2018-04-12.
+ */
+
+public class Planeta {
+
+    String nazwa;
+    String rodzaj;
+    int ksiezyce;
+
+    public Planeta(String nazwa, String rodzaj, int ksiezyce) {
+        this.nazwa = nazwa;
+        this.rodzaj = rodzaj;
+        this.ksiezyce = ksiezyce;
+    }
+
+}
+
